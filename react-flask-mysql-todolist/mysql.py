a508964de1017db213fb477dbcb8bd88a43cc224
pablokan <pablokan@gmail.com>
2020-08-01 19:06:50 -0300

    ajustes de la base de datos

--- react-flask-mysql-todolist/mysql.py
+++ react-flask-mysql-todolist/mysql.py
@@ -4,8 +4,8 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = 'root1'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'db_tasks'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
